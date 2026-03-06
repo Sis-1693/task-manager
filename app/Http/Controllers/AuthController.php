e5be6495bdf96116cab7cdbbd8d2d0dd78293c79
SalmanIbrahimShaikh <salmanshaikh1693@gmail.com>
2026-03-07 02:45:20 +0530

    fix login render database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,17 +2,29 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        if (!Auth::attempt($request->only('email', 'password')))
+        $user = User::where('email', $request->email)->first();
+
+        // अगर user database में नहीं है तो create कर दो
+        if(!$user){
+            $user = User::create([
+                'name' => 'Admin',
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+        }
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
             return response()->json(['message' => 'Invalid'], 401);
+        }
 
         $user = Auth::user();
         $token = $user->createToken('api')->plainTextToken;
